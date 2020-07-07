ecd9772bae286327d3bb44dcb6cfab73b6450acc
Richard Waller <Richard.Waller@ibm.com>
2020-07-07 22:37:58 +0100

    test: dedup tests using beforeEach

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -4,13 +4,14 @@ import { render, screen, within } from '../../test-utils';
 import App from './App';
 
 describe('App', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
   describe('when app starts', () => {
     it('renders the title for items available in the supermarket', () => {
-      render(<App />);
       expect(screen.getByText('Supermarket Items')).toBeInTheDocument();
     });
     it('renders the 3 default items in the supermarket with buttons to add them to basket', () => {
-      render(<App />);
       ['Beans', 'Coke', 'Oranges'].forEach((itemName) => {
         const shelfItem = screen.getByText(itemName);
         expect(shelfItem).toBeInTheDocument();
@@ -19,18 +20,15 @@ describe('App', () => {
       });
     });
     it('renders the "Basket" title', () => {
-      render(<App />);
       expect(screen.getByText('Basket')).toBeInTheDocument();
     });
     it('renders the default basket price (£0.00)', () => {
-      render(<App />);
       expect(screen.getByText('Total:')).toBeInTheDocument();
       expect(screen.getByText('£0.00')).toBeInTheDocument();
     });
   });
   describe('when user adds an item from shelf to the basket', () => {
     it('renders that item in the basket', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Beans');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -42,7 +40,6 @@ describe('App', () => {
       expect(basketItem).toBeInTheDocument();
     });
     it('renders a different item in the basket (with a different quantity and unit)', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Oranges');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -54,7 +51,6 @@ describe('App', () => {
       expect(basketItem).toBeInTheDocument();
     });
     it('updates the basket price', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Beans');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -66,7 +62,6 @@ describe('App', () => {
       expect(screen.getByText('£0.50')).toBeInTheDocument();
     });
     it('updates the basket price (for a different item)', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Oranges');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -78,7 +73,6 @@ describe('App', () => {
       expect(screen.getByText('£0.40')).toBeInTheDocument();
     });
     it('renders a button to add more of the item to the basket', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Beans');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -96,7 +90,6 @@ describe('App', () => {
       expect(newBasketItem).toBeInTheDocument();
     });
     it('renders a button to remove an item from the basket', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Beans');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -110,7 +103,6 @@ describe('App', () => {
       expect(basketItemDecrementButton).toBeInTheDocument();
     });
     it('renders the "Remove Item" button after the "Add Item" button', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Beans');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -127,7 +119,6 @@ describe('App', () => {
   });
   describe('when user adds multiple items to the basket', () => {
     it('renders 2 increments of an item in the basket', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Beans');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -140,7 +131,6 @@ describe('App', () => {
       expect(basketItem).toBeInTheDocument();
     });
     it('renders 2 basket items and updates the price correctly (no discount)', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Beans');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -155,7 +145,6 @@ describe('App', () => {
       expect(screen.getByText('£1.00')).toBeInTheDocument();
     });
     it('renders 2 basket items and updates the price correctly (with discount)', () => {
-      render(<App />);
       const shelfItem = screen.getByText('Coke');
       const shelfItemButton = within(shelfItem.parentElement).getByRole(
         'button',
@@ -173,7 +162,6 @@ describe('App', () => {
   describe('when user removes an item from the basket', () => {
     describe('when the basket contains just 1 increment of the item', () => {
       it('removes the item from the basket and updates the basket price', () => {
-        render(<App />);
         const shelfItem = screen.getByText('Beans');
         const shelfItemButton = within(shelfItem.parentElement).getByRole(
           'button',
@@ -193,7 +181,6 @@ describe('App', () => {
     });
     describe('when the basket contains multiple increments of the item', () => {
       it('keeps the item in the basket, updates the item quantity and basket price', () => {
-        render(<App />);
         const shelfItem = screen.getByText('Beans');
         const shelfItemButton = within(shelfItem.parentElement).getByRole(
           'button',
