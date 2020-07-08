dff1f83d4a7bd1b400c154d07fc879561ffa52d5
Richard Waller <richard.waller@ibm.com>
2020-07-08 09:39:02 +0100

    test: dedup tests using beforeEach (#2)

--- src/components/Shelf/Shelf.test.js
+++ src/components/Shelf/Shelf.test.js
@@ -3,12 +3,13 @@ import { render, screen } from '../../test-utils';
 import Shelf from './Shelf';
 
 describe('Shelf', () => {
-  it('renders a title', () => {
+  beforeEach(() => {
     render(<Shelf />);
+  });
+  it('renders a title', () => {
     expect(screen.getByText('Supermarket Items')).toBeInTheDocument();
   });
   it('renders the 3 default items', () => {
-    render(<Shelf />);
     expect(screen.getByText('Beans')).toBeInTheDocument();
     expect(screen.getByText('Coke')).toBeInTheDocument();
     expect(screen.getByText('Oranges')).toBeInTheDocument();
